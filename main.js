b35e7542b2f21d0c29f865dec391ce99fd893783
Adam Weiner <adamwineguy@gmail.com>
2014-09-15 17:27:45 -0600

    finished toArray function

---
+++ main.js
@@ -0,0 +1,58 @@
+// triple five
+
+// function triplefive() {
+// 	for (var i=0; i<3; i++) {
+// 			console.log("Five!");
+		
+// 	};
+// }
+// triplefive()
+
+
+
+
+// last letter
+
+// function lastletter(word) {
+// 	return word.charAt(word.length-1);
+
+// };
+
+// lastletter("hello");
+//lastletter("island");
+
+
+
+
+// square
+
+// var square=function(number) {
+// 	return number * number;
+
+// };
+// square(3);
+//square(5);
+
+
+
+
+// negate
+
+// var negate=function(number) {
+// 	return number * -1;
+
+// };
+// negate(5)
+// negate(8)
+
+
+
+
+// toArray
+// var toArray=function(number1, number2, number3) {
+// 	var numbers=[number1, number2, number3];
+// 		return numbers;
+
+
+// };
+// toArray(1,2,3);
