e8f884de7e5f39a66a57faadb7e55bd3e49efebd
Adam Weiner <adamwineguy@gmail.com>
2014-09-16 12:05:08 -0600

    completed function practice #1

--- main.js
+++ main.js
@@ -49,6 +49,7 @@
 
 
 // toArray
+
 // var toArray=function(number1, number2, number3) {
 // 	var numbers=[number1, number2, number3];
 // 		return numbers;
@@ -56,3 +57,95 @@
 
 // };
 // toArray(1,2,3);
+
+
+
+
+//startsWithA
+
+// var startsWithA=function(word) {
+// 	return word.charAt(0) === "a"
+// };
+
+// startsWithA("aardvark")
+// startsWithA("bitch")
+
+
+
+
+// excite
+
+// var excite=function(word) {
+// 	return word + "!!!"
+// };
+
+// excite("yes")
+// excite("no")
+
+
+
+
+// sun
+
+// var sun=function(word) {
+// 	return word.indexOf("sun") !==-1;
+// };
+
+// sun('sundries')
+// sun('bitch')
+
+
+
+
+// tiny
+
+// var tiny=function(number) {
+// 		return number>=0 && number<=1;
+// };
+
+// tiny(.3)
+// tiny(5)
+// tiny(-10)
+
+
+
+
+// getSeconds
+
+// var getSeconds=function(time) {
+// 	var parts=time.split(":")
+// 	var	minutes=+parts[0]
+// 	var	seconds=+parts[1]
+		
+// 	return minutes * 60 + seconds;
+
+// };
+
+// getSeconds('01:30')
+// getSeconds('10:25')
+
+// get seconds using parseINt
+// var getSeconds=function(time) {
+// 	var parts=time.split(":")
+// 	var minutes=parseInt(parts[0])
+// 	var seconds=parseInt(parts[1])
+// 	return minutes * 60 + seconds;
+
+// };
+
+// getSeconds('01:30')
+// getSeconds('10:25')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
